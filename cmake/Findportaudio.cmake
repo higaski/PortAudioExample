a131a5df34d618572f51ba983931a2cf6357421a
Vincent Hamp <vincent.hamp@higaski.at>
2020-07-03 16:43:22 +0200

    Build portaudio from source

--- cmake/Findportaudio.cmake
+++
@@ -1,6 +0,0 @@
-execute_process(COMMAND git submodule update --init  --recursive  --
-                                                                  external/portaudio
-                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-
-add_subdirectory(${PROJECT_SOURCE_DIR}/external/portaudio
-                 ${PROJECT_BINARY_DIR}/external/portaudio)
